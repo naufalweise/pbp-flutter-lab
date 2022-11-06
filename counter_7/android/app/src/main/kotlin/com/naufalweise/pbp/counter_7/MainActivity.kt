0818ae8ed1b15311f3494fb145959476d5bbc308
naufalweise <naufalwwidyatama@gmail.com>
2022-11-06 13:29:45 +0700

    Init commit tugas 7

---
+++ counter_7/android/app/src/main/kotlin/com/naufalweise/pbp/counter_7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.naufalweise.pbp.counter_7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ counter_7/lib/main.dart
@@ -0,0 +1,112 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Program Counter',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const CounterPage(title: 'Program Counter'),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() => _counter++);
+  }
+
+  void _decrementCounter() {
+    setState(() => {_counter = max(0, _counter - 1)});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+            child: CounterDisplay(counter: _counter,)
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Stack(
+              children: <Widget>[
+                Align(
+                    alignment: Alignment.bottomLeft,
+                    child: FloatingActionButton(
+                      onPressed: _decrementCounter,
+                      tooltip: "Decrement",
+                      child: const Icon(Icons.remove),
+                    )),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: FloatingActionButton(
+                    onPressed: _incrementCounter,
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.add),
+                  ),
+                )
+              ],
+            )));
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  const CounterDisplay({super.key, required this.counter});
+
+  final int counter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          counter.isOdd ? "GANJIL" : "GENAP",
+          style: TextStyle(color: counter.isOdd ? Colors.blue : Colors.red),
+        ),
+        Text(
+          '$counter',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ],
+    );
+  }
+}
